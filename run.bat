fcf38a6ecf2e7bc2cf0ef9bc1a3042a2b1d50973
Simone Bernardello <sbernardello@dxc.com>
2023-03-15 20:45:37 +0100

    remove dirty chars

--- run.bat
+++ run.bat
@@ -6,10 +6,10 @@ set REMOTE_HOME_FOLDER="/home/devops/"
 set COMMAND=%*
 
 docker run --name devops-tools ^
-    -it --rm ^
-    -v "%WORKSPACE%":"%WORKSPACE_MOUNT_POINT%" ^
-    -v "%LOCAL_CONFIG_FOLDER%/.kube":"%REMOTE_HOME_FOLDER%/.kube" ^
-    -v "%LOCAL_CONFIG_FOLDER%/.awsvault":"/home/devops/.awsvault" ^
-    -v "%LOCAL_CONFIG_FOLDER%/.aws":"/home/devops/.aws" ^
-    -e AWS_VAULT_BACKEND=file ^
-    devops-tools bash -c "%COMMAND%"
+    -it --rm ^
+    -v "%WORKSPACE%":"%WORKSPACE_MOUNT_POINT%" ^
+    -v "%LOCAL_CONFIG_FOLDER%/.kube":"%REMOTE_HOME_FOLDER%/.kube" ^
+    -v "%LOCAL_CONFIG_FOLDER%/.awsvault":"/home/devops/.awsvault" ^
+    -v "%LOCAL_CONFIG_FOLDER%/.aws":"/home/devops/.aws" ^
+    -e AWS_VAULT_BACKEND=file ^
+    devops-tools bash -c "%COMMAND%"
